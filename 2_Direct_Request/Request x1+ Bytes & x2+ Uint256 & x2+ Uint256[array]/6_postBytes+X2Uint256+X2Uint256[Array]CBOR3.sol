a801996126e59fbf70ff0be3872d00b6e19e4005
mgladson <99776898+mgladson@users.noreply.github.com>
2022-06-16 09:28:08 -0700

    Add files via upload

---
+++ 2_Direct_Request/Request x1+ Bytes & x2+ Uint256 & x2+ Uint256[array]/6_postBytes+X2Uint256+X2Uint256[Array]CBOR3.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+// block-farms.io
+// Discord=https://discord.gg/PgxRVrDUm7
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract MultiDataTypeRequest is ChainlinkClient {
+  using Chainlink for Chainlink.Request;
+
+  bytes public data;
+  string public convertedString;
+  uint256 public number1;
+  uint256 public number2;
+  uint256[] public array1;
+  uint256[] public array2;
+
+  uint256 constant private ORACLE_PAYMENT = 0 * LINK_DIVISIBILITY / 100 * 5;
+
+  constructor(
+  ) {
+    setChainlinkToken(LINK_TOKEN_ADDRESS);
+    setChainlinkOracle(OPERATOR_ADDRESS);
+  }
+
+
+  function requestMultiVariable(
+  )
+    public
+  {
+    bytes32 externalJobId = "job_id";
+    Chainlink.Request memory req = buildChainlinkRequest(externalJobId, address(this), this.fulfillRequest.selector);
+    req.add("input1", "headerInput1Value");
+    req.add("input2", "headerInput2Value");
+    req.add("input3", "headerInput3Value");
+    req.add("path1", "data,path1");
+    req.add("path2", "data,path2");
+    req.add("path3", "data,path3");
+    req.add("path4", "data,path4");
+    req.add("path5", "data,path5");
+    req.add("times", 100);
+    sendOperatorRequest(req, ORACLE_PAYMENT);
+  }
+
+  event RequestFulfilled(
+    bytes32 indexed requestId,
+    bytes indexed data,
+    uint256 number1,
+    uint256 number2,
+    uint256[] array1,
+    uint256[] array2
+  );
+
+
+  function fulfillRequest(
+    bytes32 requestId,
+    bytes memory bytesData,
+    uint256 _number1,
+    uint256 _number2,
+    uint256[] memory _array1,
+    uint256[] memory _array2
+  )
+    public
+    recordChainlinkFulfillment(requestId)
+  {
+    emit RequestFulfilled(requestId, bytesData, _number1, _number2, _array2, _array1);
+    data = bytesData;
+    convertedString = string(data);
+    number1 = _number1;
+    number2 = _number2;
+    array2 = _array2;
+    array1 = _array1;
+  }
+
+}
