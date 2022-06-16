7abdb6523186c49d0fec7f9a79315a04d265d2e0
mgladson <99776898+mgladson@users.noreply.github.com>
2022-06-16 01:12:19 -0700

    Add files via upload

---
+++ 2_Direct_Request/Request x1 Uint256/5_postUint256NoMultiplyCBOR1.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+// block-farms.io
+// Discord=https://discord.gg/PgxRVrDUm7
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+contract getUintTemplate is ChainlinkClient, ConfirmedOwner {
+  using Chainlink for Chainlink.Request;
+
+  uint256 constant private ORACLE_PAYMENT = 0 * LINK_DIVISIBILITY;
+  uint256 public Uint;
+  address private _oracle = ORACLE_CONTRACT_ADDRESS;
+  bytes32 private _jobId = "JOB_ID";
+
+  event RequestUintFulfilled(
+    bytes32 indexed requestId,
+    uint256 indexed Uint
+  );
+
+  constructor() ConfirmedOwner(msg.sender){
+    setPublicChainlinkToken();
+  }
+
+  function requestUint()
+    public
+    onlyOwner
+  {
+    Chainlink.Request memory req = buildChainlinkRequest(_jobId, address(this), this.fulfillUint.selector);
+    req.add("input", "inputVariable");
+    req.add("path", "data,results");
+    sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
+  }
+
+  function fulfillUint(bytes32 _requestId, uint256 _Uint)
+    public
+    recordChainlinkFulfillment(_requestId)
+  {
+    emit RequestUintFulfilled(_requestId, _Uint);
+    Uint = _Uint;
+  }
+
+}
