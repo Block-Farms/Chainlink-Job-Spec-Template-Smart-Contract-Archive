f5ba898ab16a10ca7d9d7beca3f13d2c1755db51
mgladson <99776898+mgladson@users.noreply.github.com>
2022-06-16 09:01:50 -0700

    Add files via upload

---
+++ 2_Direct_Request/Request x1+ Bytes & x2+ Uint256/0_postBytes+X2Uint256.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+// block-farms.io
+// Discord=https://discord.gg/PgxRVrDUm7
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract MultiDataTypeRequest is ChainlinkClient {
+  using Chainlink for Chainlink.Request;
+
+
+  bytes public data;
+  string public convertedString;
+  uint256 public number1;
+  uint256 public number2;
+
+  uint256 constant private ORACLE_PAYMENT = 0 * LINK_DIVISIBILITY / 100 * 5;
+
+  constructor(
+  ) {
+    setChainlinkToken(LINK_TOKEN_ADDRESS);
+    setChainlinkOracle(OPERATOR_ADDRESS);
+  }
+
+
+  function requestMultiVariable(
+  )
+    public
+  {
+    bytes32 externalJobId = "job_id";
+    Chainlink.Request memory req = buildChainlinkRequest(externalJobId, address(this), this.fulfillBytesAndUint.selector);
+    req.add("get", "API_endpoint_url");
+    req.add("path1", "data,path1");
+    req.add("path2", "data,path2");
+    req.add("path3", "data,path3");
+    req.add("times", 100);
+    sendOperatorRequest(req, ORACLE_PAYMENT);
+  }
+
+  event RequestFulfilled(
+    bytes32 indexed requestId,
+    bytes indexed data,
+    uint256 number1,
+    uint256 number2
+  );
+
+
+  function fulfillBytesAndUint(
+    bytes32 requestId,
+    bytes memory bytesData,
+    uint256 _number1,
+    uint256 _number2
+  )
+    public
+    recordChainlinkFulfillment(requestId)
+  {
+    emit RequestFulfilled(requestId, bytesData, _number1, _number2);
+    data = bytesData;
+    convertedString = string(data);
+    number1 = _number1;
+    number2 = _number2;
+  }
+
+}
