b65a21a079c8fadfdb1c43f039bcfab1c17edd66
mgladson <99776898+mgladson@users.noreply.github.com>
2022-06-16 14:47:19 -0700

    Add files via upload

---
+++ 2_Direct_Request/Request x1 Bytes/0_getBool.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+// block-farms.io
+// Discord=https://discord.gg/PgxRVrDUm7
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+contract getBoolTemplate is ChainlinkClient, ConfirmedOwner {
+  using Chainlink for Chainlink.Request;
+
+  uint256 constant private ORACLE_PAYMENT = 0 * LINK_DIVISIBILITY;
+  bool public boolean;
+  bytes32 private _jobId = "JOB_ID";
+
+  event RequestBoolFulfilled(
+    bytes32 indexed requestId,
+    uint256 indexed boolean
+  );
+
+  constructor() ConfirmedOwner(msg.sender){
+  setChainlinkToken(LINK_TOKEN_ADDRESS);
+  setChainlinkOracle(OPERATOR_ADDRESS);
+  }
+
+  function requestBool()
+    public
+    onlyOwner
+  {
+    Chainlink.Request memory req = buildChainlinkRequest(_jobId, address(this), this.fulfillBool.selector);
+    req.add("get", "YOUR_API_ENDPOINT_URL");
+    req.add("path", "data,results");
+    sendChainlinkRequestTo(req, ORACLE_PAYMENT);
+  }
+
+  function fulfillBool(bytes32 _requestId, bool _boolean)
+    public
+    recordChainlinkFulfillment(_requestId)
+  {
+    emit RequestUintFulfilled(_requestId, _boolean);
+    boolean = _boolean;
+  }
+
+}
