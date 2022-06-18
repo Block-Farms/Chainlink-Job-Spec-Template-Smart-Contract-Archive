494e02dd3de28c7cf70298a6357c3b76e172f77a
mgladson <99776898+mgladson@users.noreply.github.com>
2022-06-18 05:51:00 -0700

    Update 4_postX2Uint256CBOR1.sol

--- 2_Direct_Request/Request x2+ Uint256/4_postX2Uint256CBOR1.sol
+++ 2_Direct_Request/Request x2+ Uint256/4_postX2Uint256CBOR1.sol
@@ -28,9 +28,10 @@ contract MultiVariableRequest is ChainlinkClient, ConfirmedOwner {
     onlyOwner
   {
     Chainlink.Request memory req = buildChainlinkRequest(externalJobId, address(this), this.fulfillValue1AndValue2.selector);
-    req.add("input", "inputVariable1");
+    req.add("input1", "inputVariable1");
     req.add("path1", "data,results1");
     req.add("path2", "data,results2");
+    req.add("times", 100);
     sendOperatorRequest(req, oraclePayment);
   }
 
