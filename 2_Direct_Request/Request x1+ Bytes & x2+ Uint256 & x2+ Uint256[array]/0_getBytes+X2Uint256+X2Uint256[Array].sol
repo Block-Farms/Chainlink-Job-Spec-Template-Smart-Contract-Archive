fc187192c3f29e329720159b1fc9bcf79cbd8562
mgladson <99776898+mgladson@users.noreply.github.com>
2022-06-18 05:16:39 -0700

    Update 0_getBytes+X2Uint256+X2Uint256[Array].sol

--- 2_Direct_Request/Request x1+ Bytes & x2+ Uint256 & x2+ Uint256[array]/0_getBytes+X2Uint256+X2Uint256[Array].sol
+++ 2_Direct_Request/Request x1+ Bytes & x2+ Uint256 & x2+ Uint256[array]/0_getBytes+X2Uint256+X2Uint256[Array].sol
@@ -13,15 +13,16 @@ contract MultiDataTypeRequest is ChainlinkClient {
   string public convertedString;
   uint256 public number1;
   uint256 public number2;
-  uint256[] public array1;
-  uint256[] public array2;
 
-  uint256 constant private ORACLE_PAYMENT = 0 * LINK_DIVISIBILITY / 100 * 5;
+  bytes32 private externalJobId;
+  uint256 private oraclePayment;
 
   constructor(
   ) {
     setChainlinkToken(LINK_TOKEN_ADDRESS);
     setChainlinkOracle(OPERATOR_ADDRESS);
+    externalJobId = "externalJobId";
+    oraclePayment = (0.0 * LINK_DIVISIBILITY); // n * 10**18
   }
 
 
@@ -29,46 +30,38 @@ contract MultiDataTypeRequest is ChainlinkClient {
   )
     public
   {
-    bytes32 externalJobId = "job_id";
-    Chainlink.Request memory req = buildChainlinkRequest(externalJobId, address(this), this.fulfillRequest.selector);
-    req.add("get", "API_endpoint_url");
+
+    Chainlink.Request memory req = buildChainlinkRequest(externalJobId, address(this), this.fulfillBytesAndUint.selector);
+    req.add("get", "https://API_endpoint_url");
     req.add("path1", "data,path1");
     req.add("path2", "data,path2");
     req.add("path3", "data,path3");
-    req.add("path4", "data,path4");
-    req.add("path5", "data,path5");
     req.add("times", 100);
-    sendOperatorRequest(req, ORACLE_PAYMENT);
+    sendOperatorRequest(req, oraclePayment);
   }
 
   event RequestFulfilled(
     bytes32 indexed requestId,
     bytes indexed data,
     uint256 number1,
-    uint256 number2,
-    uint256[] array1,
-    uint256[] array2
+    uint256 number2
   );
 
 
-  function fulfillRequest(
+  function fulfillBytesAndUint(
     bytes32 requestId,
     bytes memory bytesData,
     uint256 _number1,
-    uint256 _number2,
-    uint256[] memory _array1,
-    uint256[] memory _array2
+    uint256 _number2
   )
     public
     recordChainlinkFulfillment(requestId)
   {
-    emit RequestFulfilled(requestId, bytesData, _number1, _number2, _array2, _array1);
+    emit RequestFulfilled(requestId, bytesData, _number1, _number2);
     data = bytesData;
     convertedString = string(data);
     number1 = _number1;
     number2 = _number2;
-    array2 = _array2;
-    array1 = _array1;
   }
 
 }
