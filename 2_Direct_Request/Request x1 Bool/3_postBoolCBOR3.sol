6cdec755ae97662b4739d7bc8b76f3651da6286f
hanna004 <100059207+hanna004@users.noreply.github.com>
2022-07-02 02:44:02 -0700

    Update 3_postBoolCBOR3.sol

--- 2_Direct_Request/Request x1 Bool/3_postBoolCBOR3.sol
+++ 2_Direct_Request/Request x1 Bool/3_postBoolCBOR3.sol
@@ -14,15 +14,13 @@ contract getBoolTemplate is ChainlinkClient, ConfirmedOwner {
   
   bytes32 private externalJobId;
   uint256 private oraclePayment;
+  address private oracle;
 
-  event RequestBoolFulfilled(
-    bytes32 indexed requestId,
-    uint256 indexed boolean
-  );
+  event RequestBoolFulfilled(bytes32 indexed requestId,bool indexed boolean);
 
   constructor() ConfirmedOwner(msg.sender){
-  setChainlinkToken(LINK_TOKEN_ADDRESS);
-  setChainlinkOracle(OPERATOR_ADDRESS);
+  setChainlinkToken(0xa36085F69e2889c224210F603D836748e7dC0088);
+  oracle = 0x7AE841fCD06a794537912749C5449b3290de780f;
   externalJobId = "externalJobId";
   oraclePayment = (0.0 * LINK_DIVISIBILITY); // n * 10**18
   }
@@ -36,14 +34,14 @@ contract getBoolTemplate is ChainlinkClient, ConfirmedOwner {
     req.add("input2", "inputVariable2");
     req.add("input3", "inputVariable3");
     req.add("path", "data,results");
-    sendChainlinkRequestTo(req, oraclePayment);
+    sendChainlinkRequestTo(oracle, req, oraclePayment);
   }
 
   function fulfillBool(bytes32 _requestId, bool _boolean)
     public
     recordChainlinkFulfillment(_requestId)
   {
-    emit RequestUintFulfilled(_requestId, _boolean);
+    emit RequestBoolFulfilled(_requestId, _boolean);
     boolean = _boolean;
   }
 
