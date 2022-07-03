908ce93e1dae55fd6fd410eaf3e342055985dcea
mgladson <99776898+mgladson@users.noreply.github.com>
2022-07-03 01:52:11 -0700

    Update 0_getBytes+X2Uint256+X2Uint256[Array].sol

--- 2_Direct_Request/Request x1+ Bytes & x2+ Uint256 & x2+ Uint256[array]/0_getBytes+X2Uint256+X2Uint256[Array].sol
+++ 2_Direct_Request/Request x1+ Bytes & x2+ Uint256 & x2+ Uint256[array]/0_getBytes+X2Uint256+X2Uint256[Array].sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
-contract MultiDataTypeRequest is ChainlinkClient {
+contract getMultiDataTypeTemplate is ChainlinkClient {
   using Chainlink for Chainlink.Request;
 
   bytes public data;
@@ -40,20 +40,9 @@ contract MultiDataTypeRequest is ChainlinkClient {
     sendOperatorRequest(req, oraclePayment);
   }
 
-  event RequestFulfilled(
-    bytes32 indexed requestId,
-    bytes indexed data,
-    uint256 number1,
-    uint256 number2
-  );
+  event RequestFulfilled(bytes32 indexed requestId, bytes indexed data, uint256 number1, uint256 number2);
 
-
-  function fulfillBytesAndUint(
-    bytes32 requestId,
-    bytes memory bytesData,
-    uint256 _number1,
-    uint256 _number2
-  )
+  function fulfillBytesAndUint(bytes32 requestId, bytes memory bytesData, uint256 _number1,uint256 _number2)
     public
     recordChainlinkFulfillment(requestId)
   {
