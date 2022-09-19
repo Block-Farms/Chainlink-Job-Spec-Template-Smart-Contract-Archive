4d2608d6e88fef450d04fd9ba1192a738be27825
mgladson <99776898+mgladson@users.noreply.github.com>
2022-09-19 04:00:13 -0700

    Create 0_getInt.sol

---
+++ 2_Direct_Request/Request x1 Int256/0_getInt.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+// block-farms.io
+// Discord=https://discord.gg/PgxRVrDUm7
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+contract getUintTemplate is ChainlinkClient, ConfirmedOwner {
+  using Chainlink for Chainlink.Request;
+
+  int256 public int;
+  
+  bytes32 private externalJobId;
+  int256 private oraclePayment;
+  address private oracle;
+  
+  event RequestIntFulfilled(bytes32 indexed requestId,int256 indexed int);
+
+  constructor() ConfirmedOwner(msg.sender){
+    setChainlinkToken(LINK_TOKEN_ADDRESS);
+    oracle = ORACLE_ADDRESS;
+    externalJobId = "externalJobId";
+    oraclePayment = (0.0 * LINK_DIVISIBILITY); // n * 10**18
+  }
+
+  function requestInt()
+    public
+    onlyOwner
+  {
+    Chainlink.Request memory req = buildChainlinkRequest(externalJobId, address(this), this.fulfillInt.selector);
+    req.add("get", "https://YOUR_API_ENDPOINT_URL");
+    req.add("path", "data,results");
+    req.addInt("times", 100);
+    sendChainlinkRequestTo(oracle, req, oraclePayment);
+  }
+
+  function fulfillInt(bytes32 _requestId, int256 _Int)
+    public
+    recordChainlinkFulfillment(_requestId)
+  {
+    emit RequestIntFulfilled(_requestId, _Int);
+    Int = _Int;
+  }
+
+}
